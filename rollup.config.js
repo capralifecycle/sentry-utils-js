4379e94022c4d04056a858df7446eda3f4566961
Øyvind Moldestad <moldestad@gmail.com>
2019-11-08 13:13:23 +0100

    fix(build): Fix build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,30 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
-const external = [
-  ...Object.keys(pkg.dependencies || {}),
-  ...Object.keys(pkg.peerDependencies || {})
-];
-
-const plugins = [typescript()];
-
-export default [
-  // CommonJS
-  {
-    input: 'src/index.ts',
-    output: {
-      file: pkg.main,
-      format: 'cjs'
+export default {
+  input: 'src/index.ts',
+  output: [
+    // CommonJS
+    {
+      output: {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      }
     },
-    external,
-    plugins
-  },
-  // ES
-  {
-    input: 'src/index.ts',
-    output: {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    },
-    external,
-    plugins
-  }
-];
+    // ES
+    {
+      input: 'src/index.ts',
+      output: {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true
+      }
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
+  plugins: [typescript()]
+};
